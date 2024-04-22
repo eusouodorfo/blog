d4550c25db9d60b49c690fe33279ac29786abdb5
Rodolfo "Dorfo" N. Silva <rodolfo.dorfo@outlook.com>
2024-04-22 00:39:58 -0300

    home quase pronta

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php
+    include_once("templates/header.php")
+?>
+
+<body>
+    <h1>Contato</h1>
+
+<?php
+    include_once("templates/footer.php")
+?>
---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p> Platinum Slime Studios &copy; 2024</p>
+</footer>
+</body>
+</html>
